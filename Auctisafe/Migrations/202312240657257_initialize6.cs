30a8b57243e826560f76eda7041e2c5dd2a1436d
Abdul Qadir Abdul Latif <3054359+abdulahadabro@users.noreply.github.com>
2023-12-24 11:58:22 +0500

    some updates

---
+++ Auctisafe/Migrations/202312240657257_initialize6.cs
@@ -0,0 +1,27 @@
+﻿namespace Auctisafe.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class initialize6 : DbMigration
+    {
+        public override void Up()
+        {
+            DropTable("dbo.Winners");
+        }
+        
+        public override void Down()
+        {
+            CreateTable(
+                "dbo.Winners",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        Email = c.String(),
+                        Message = c.String(),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+        }
+    }
+}
